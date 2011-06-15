d73bce9360e110daa3cd1be192d1b1f9f1c87258
zny2008 <zny2008@02dc478b-fe90-6d34-b7c4-bbeca2ecd945>
2011-06-15 03:00:22 +0000

    fsm_achieve.cpp 增加

--- src/frame/src/comm/fsm/fsm_achieve.h
+++ src/frame/src/comm/fsm/fsm_achieve.h
@@ -55,211 +55,55 @@ typedef struct _StFsmNode
     }
 } StFsmNode;
 
-/**
-* @brief    映射时间到统计
-*
-* @param    msec
-* @param    baseLine
-*
-* @return   
-*/
-static int MapTime2StatIndex(int msec,int baseLine)
-{
-    int statTimeIndex;
-    if(msec<5)
-        statTimeIndex = baseLine;
-    else if(msec<10)
-        statTimeIndex = baseLine+1;
-    else if(msec<50)
-        statTimeIndex = baseLine+2;
-    else if(msec<100)
-        statTimeIndex = baseLine+3;
-    else if(msec<200)
-        statTimeIndex = baseLine+4;
-    else if(msec<500)
-        statTimeIndex = baseLine+5;
-    else if(msec<1000)
-        statTimeIndex = baseLine+6;
-    else if(msec<2000)
-        statTimeIndex = baseLine+7;
-    else if(msec<3000)
-        statTimeIndex = baseLine+8;
-    else if(msec<5000)
-        statTimeIndex = baseLine+9;
-    else
-        statTimeIndex = baseLine+10;
-    return statTimeIndex;
-}
-
-
 class CFrameBase : public IFrame
 {
 public:
-    CFrameBase () {
-        m_needGCCount = 0;
-        m_allActorCount = 0;
-    }
-    virtual ~CFrameBase () {}
+    CFrameBase ();
+    virtual ~CFrameBase ();
 
-    int Init(const char* statDir, const char* statFileName)
-    {
-        int ret = m_dirStat.Init(statDir, statFileName,stat_desc, STAT_OVER);
-        if (ret != 0)
-        {
-            return -1;
-        }
+    /**
+     * @brief   初始化
+     *
+     * @param   statDir             统计文件目录
+     * @param   statFileName        统计文件名
+     *
+     * @return  0                   succ
+     */
+    int Init(const char* statDir, const char* statFileName);
 
-        vector<string> vecKey1,vecKey2;
-        m_dirStat.ResetStat(vecKey1,vecKey2);
-        return 0;
-    }
+    int AddActor(IActor* pActor);
 
-    int AddActor(IActor* pActor)
-    {
-        m_listActors.push_front(pActor);
-        m_allActorCount++;
-        StatAddCount("ALL","SELF",STAT_ALIVE);
-        StatAddCount("ALL","SELF",STAT_TOTAL);
+    int GetActorCount();
 
-        StatAddCount("ALL","VALID",STAT_ALIVE);
-        StatAddCount("ALL","VALID",STAT_TOTAL);
+    int AddNeedGCCount();
 
-        StatAddCount(pActor->Name().c_str(),"SELF",STAT_ALIVE);
-        StatAddCount(pActor->Name().c_str(),"SELF",STAT_TOTAL);
+    int SubNeedGCCount();
 
-        StatAddCount(pActor->Name().c_str(),"VALID",STAT_ALIVE);
-        StatAddCount(pActor->Name().c_str(),"VALID",STAT_TOTAL);
+    int GetNeedGCCount();
 
-        return 0;
-    }
-    int GetActorCount()
-    {
-        return m_allActorCount;
-    }
-    int AddNeedGCCount()
-    {
-        m_needGCCount++;
-        StatAddCount("GC","SELF",STAT_ALIVE);
-        StatAddCount("GC","SELF",STAT_TOTAL);
-        return 0;
-    }
-    int SubNeedGCCount()
-    {
-        m_needGCCount--;
-        if (m_needGCCount < 0)
-        {
-            m_needGCCount = 0;
-        }
-        StatDecCount("GC","SELF",STAT_ALIVE);
-        return 0;
-    }
-    int GetNeedGCCount()
-    {
-        return m_needGCCount;
-    }
-    void GCActors()
-    {
-        for(list<IActor*>::iterator it = m_listActors.begin(); it != m_listActors.end();)
-        {
-            //list<IActor*>::iterator tempIt = it;
-            //it++;
+    void GCActors();
 
-            if ((*it)->GetGCMark())
-            {
-                it = eraseActor(it);
-            }
-            else
-            {
-                ++it;
-            }
-        }
-    }
-    map<int, IFsm*>* GetFsmMgr()
-    {
-        return &m_mapFsmMgr;
-    }
-    int RegFsm(int state, IFsm* fsm)
-    {
-        if (state <=0 || fsm == NULL)
-        {
-            return -1;
-        }
-        fsm->SetFrame(this);
-        fsm->SetStateID(state);
-        m_mapFsmMgr[state] = fsm;
-        return 0;
-    }
-    virtual void StatChangeFsm(IFsm *fsm, IActor* pActor, const string &strFsmFunc)
-    {
-        if (fsm == NULL || pActor == NULL)
-        {
-            return;
-        }
-        if (strFsmFunc == "Entry")
-        {
-            pActor->StatFsmEntry(fsm);
+    map<int, IFsm*>* GetFsmMgr();
 
-            StatAddCount("ALL",fsm->Name().c_str(), STAT_ALIVE);
-            StatAddCount("ALL",fsm->Name().c_str(), STAT_TOTAL);
+    int RegFsm(int state, IFsm* fsm);
 
-            StatAddCount(pActor->Name().c_str(),fsm->Name().c_str(), STAT_ALIVE);
-            StatAddCount(pActor->Name().c_str(),fsm->Name().c_str(), STAT_TOTAL);
 
-        }
-        else if (strFsmFunc == "Exit")
-        {
-            int pastTimeMs = 0;
-            pActor->StatFsmExit(fsm,pastTimeMs);
+    virtual void StatChangeFsm(IFsm *fsm, IActor* pActor, const string &strFsmFunc);
 
-            int statTimeIndex = MapTime2StatIndex(pastTimeMs, STAT_5MS_REQ);
-            //统计fsm的时间
-            StatAddCount("ALL",fsm->Name().c_str(), statTimeIndex);
-            StatAddCount(pActor->Name().c_str(),fsm->Name().c_str(), statTimeIndex);
+    virtual int StatAddCount(const char* key1, const char* key2, int index);
 
-            StatDecCount("ALL",fsm->Name().c_str(), STAT_ALIVE);
-            StatDecCount(pActor->Name().c_str(),fsm->Name().c_str(), STAT_ALIVE);
-        }
-    }
-
-    virtual int StatAddCount(const char* key1, const char* key2, int index)
-    {
-        return m_dirStat.AddCount(key1,key2,index);
-    }
-
-    virtual int StatDecCount(const char* key1, const char* key2, int index)
-    {
-        return m_dirStat.DecCount(key1,key2,index);
-    }
+    virtual int StatDecCount(const char* key1, const char* key2, int index);
 
 protected:
     /**
      * @brief   删除一个actor的指针
      *
-     * @param   it
+     * @param   it                  actor的stl指针
      *
-     * @return  
+     * @return  0                   succ
      */
-    list<IActor*>::iterator eraseActor(list<IActor*>::iterator it)
-    {
-        StatDecCount("ALL","SELF",STAT_ALIVE);
-        StatDecCount((*it)->Name().c_str(),"SELF",STAT_ALIVE);
+    list<IActor*>::iterator eraseActor(list<IActor*>::iterator it);
 
-        //统计存活时间和GC时间
-        int statTimeIndex = MapTime2StatIndex((*it)->GetAliveTimeMs(), STAT_5MS_REQ);
-        StatAddCount("ALL","SELF",statTimeIndex);
-        StatAddCount((*it)->Name().c_str(),"SELF",statTimeIndex);
-
-        statTimeIndex = MapTime2StatIndex((*it)->GetGCTimeMs(),STAT_5MS_REQ);
-        StatAddCount("GC","SELF",statTimeIndex);
-
-
-        SubNeedGCCount();
-
-        delete (*it);
-        list<IActor*>::iterator next_it = m_listActors.erase(it);
-        m_allActorCount--;
-        return next_it;
-    }
 protected:
     list<IActor*> m_listActors;
 
@@ -276,170 +120,39 @@ protected:
 class CActorBase : public IActor, public IPtrProxy<CActorBase>
 {
 public:
-    CActorBase () {
-        m_bGC = false;
-        m_Fsm = NULL;
-        m_ptrMapFsmMgr = NULL;
-        m_pFrame = NULL;
+    CActorBase ();
+    virtual ~CActorBase ();
 
-        m_aliveTimer.Start();
-    }
+    int AttachFrame(IFrame* pFrame);
 
-    virtual ~CActorBase () {}
+    IFrame* GetFrame();
 
-    void SetGCMark()
-    {
-        if (m_bGC)
-        {
-            return;
-        }
-        m_bGC = true;
-        m_gcTimer.Start();
-        if (m_pFrame)
-        {
-            m_pFrame->AddNeedGCCount();
+    void SetGCMark();
 
-            //统计有效的数量
-            m_pFrame->StatDecCount("ALL","VALID",STAT_ALIVE);
-            m_pFrame->StatDecCount(Name().c_str(),"VALID",STAT_ALIVE);
+    bool GetGCMark();
 
-            int pastTimeMs = m_aliveTimer.GetPastTime();
-            int statTimeIndex = MapTime2StatIndex(pastTimeMs, STAT_5MS_REQ);
-            m_pFrame->StatAddCount("ALL","VALID",statTimeIndex);
-            m_pFrame->StatAddCount(Name().c_str(),"VALID",statTimeIndex);
-        }
-    }
-    bool GetGCMark()
-    {
-        return m_bGC;
-    }
-    int AttachFrame(IFrame* pFrame)
-    {
-        if (pFrame == NULL)
-        {
-            return -1;
-        }
-        m_pFrame = pFrame;
-        m_pFrame->AddActor(this);
-        SetFsmMgr(m_pFrame->GetFsmMgr());
-        return 0;
-    }
-    IFrame* GetFrame()
-    {
-        return m_pFrame;
-    }
+    void SetFsmMgr(map<int, IFsm*> * ptrMapFsmMgr);
 
-    void SetFsmMgr(map<int, IFsm*> * ptrMapFsmMgr)
-    {
-        m_ptrMapFsmMgr = ptrMapFsmMgr;
-    }
-    map<int, IFsm*>* GetFsmMgr()
-    {
-        return m_ptrMapFsmMgr;
-    }
+    map<int, IFsm*>* GetFsmMgr();
 
-    int ProcessState()
-    {
-        if (m_ptrMapFsmMgr == NULL)
-        {
-            return -1;
-        }
-        int state = doChangeFsm(m_Fsm);
-        return ChangeState(state);
-    }
+    int ProcessState();
 
-    int ChangeState(int destState)
-    {
-        if (m_ptrMapFsmMgr == NULL)
-        {
-            return -1;
-        }
+    int ChangeState(int destState);
 
-        if (0 == destState)
-        {
-            //此次处理结束,但是并没有退出这个状态
-            return 0;
-        }
-        else if (destState < 0)
-        {
-            //需要关闭整个请求
-            if (m_Fsm)
-            {
-                m_Fsm->Exit(this);
-                m_Fsm = NULL;
-            }
-            return destState;
-        }
-        IFsm * destFsm = NULL;
-        destFsm = (*m_ptrMapFsmMgr)[destState];
-        int state = doChangeFsm(destFsm);
-        return ChangeState(state);
-    }
-    int HandleEvent(IEvent* pEvent)
-    {
-        //默认是什么也不做的，继承的类如果需要用到，就要重写
-        return 0;
-    }
+    int HandleEvent(IEvent* pEvent);
 
-    void ClearFsmNodes()
-    {
-        m_vecFsmNodes.clear();
-    }
+    void ClearFsmNodes();
 
-    virtual int StatFsmEntry(IFsm* fsm)
-    {
-        StFsmNode node;
-        node.fsm = fsm;
-        m_vecFsmNodes.push_back(node);
-        return 0;
-    }
+    int StatFsmEntry(IFsm* fsm);
 
-    virtual int StatFsmExit(IFsm* fsm, int& pastTimeMs)
-    {
-        if (m_vecFsmNodes.empty())
-        {
-            return -1;
-        }
-        StFsmNode& node = m_vecFsmNodes.back();
-        if (node.fsm != fsm)
-        {
-            return -2;
-        }
+    int StatFsmExit(IFsm* fsm, int& pastTimeMs);
 
-        node.Stop();
-        pastTimeMs = node.GetPastTime();
-        return 0;
-    }
+    int GetAliveTimeMs();
 
-    int GetAliveTimeMs()
-    {
-        return m_aliveTimer.GetPastTime();
-    }
-
-    int GetGCTimeMs()
-    {
-        return m_gcTimer.GetPastTime();
-    }
+    int GetGCTimeMs();
 
 private:
-    int doChangeFsm(IFsm* destFsm)
-    {
-        if (destFsm == NULL)
-        {
-            return 0;
-        }
-
-        if (m_Fsm != destFsm)
-        {
-            if (m_Fsm != NULL)
-            {
-                m_Fsm->Exit(this);
-            }
-            m_Fsm = destFsm;
-            m_Fsm->Entry(this);
-        }
-        return m_Fsm->Process(this);
-    }
+    int doChangeFsm(IFsm* destFsm);
 
 protected:
     bool m_bGC;
@@ -454,52 +167,20 @@ protected:
 class CFsmBase : public IFsm
 {
 public:
-    CFsmBase () {
-        m_pFrame = NULL;
-        m_stateID = -1;
-    }
-    virtual ~CFsmBase () {}
-    void SetFrame(IFrame* pFrame)
-    {
-        m_pFrame = pFrame;
-    }
+    CFsmBase ();
+    virtual ~CFsmBase ();
 
-    void SetStateID(int state)
-    {
-        m_stateID = state;
-    }
+    void SetFrame(IFrame* pFrame);
 
-    int GetStateID()
-    {
-        return m_stateID;
-    }
+    void SetStateID(int state);
 
-    int Entry(IActor* pActor)
-    {
-        if (m_pFrame)
-        {
-            m_pFrame->StatChangeFsm(this, pActor, __func__);
-        }
-        return HandleEntry(pActor);
-    }
+    int GetStateID();
 
-    int Process(IActor* pActor)
-    {
-        if (m_pFrame)
-        {
-            m_pFrame->StatChangeFsm(this, pActor, __func__);
-        }
-        return HandleProcess(pActor);
-    }
+    int Entry(IActor* pActor);
 
-    int Exit(IActor* pActor)
-    {
-        if (m_pFrame)
-        {
-            m_pFrame->StatChangeFsm(this, pActor, __func__);
-        }
-        return HandleExit(pActor);
-    }
+    int Process(IActor* pActor);
+
+    int Exit(IActor* pActor);
 
     /**
      * @brief   在进入这个状态的时候，pActor需要做的事情
