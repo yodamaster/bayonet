a0037288251bd86196f06ffe15f9413b8f775e36
zny2008 <zny2008@02dc478b-fe90-6d34-b7c4-bbeca2ecd945>
2011-06-16 03:28:52 +0000

    长连接

--- src/svr2/main.cpp
+++ src/svr2/main.cpp
@@ -219,7 +219,7 @@ int main(int argc, const char *argv[])
     param.infoDir="bayonet";
     param.ip="0.0.0.0";
     param.port = 10001;
-    //param.bKeepcnt= true;
+    param.bKeepcnt= true;
     //param.protoType = PROTO_TYPE_UDP;
     param.protoType = PROTO_TYPE_TCP;
     param.pAction = new CActionFirst();
