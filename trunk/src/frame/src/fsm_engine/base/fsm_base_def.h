646e05db0b6904a76ca2769d291708d05a21467d
zny2008 <zny2008@02dc478b-fe90-6d34-b7c4-bbeca2ecd945>
2011-06-29 15:59:36 +0000

    fsm引擎的定义文件

---
+++ trunk/src/frame/src/fsm_engine/base/fsm_base_def.h
@@ -0,0 +1,26 @@
+/*=============================================================================
+#
+#     FileName: fsm_base_def.h
+#         Desc: 定义了fsm引擎的一些基本定义
+#
+#       Author: dantezhu
+#        Email: zny2008@gmail.com
+#     HomePage: http://www.vimer.cn
+#
+#      Created: 2011-06-29 23:56:52
+#      Version: 0.0.1
+#      History:
+#               0.0.1 | dantezhu | 2011-06-29 23:56:52 | initialization
+#
+=============================================================================*/
+#ifndef __FSM_BASE_DEF_H_20110629235706__
+#define __FSM_BASE_DEF_H_20110629235706__
+//统计等级，数字越大，性能越差
+//测试过，能让性能降低一半
+enum EnumStatLevel
+{
+    EnumStatLevelNone  = 0,             //所有的统计都关闭
+    EnumStatLevelBrief = 1,             //统计所有的SELF和VALID
+    EnumStatLevelFull  = 2,             //所有统计
+};
+#endif
