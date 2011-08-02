d8b31822e36c47644a4cd2b261a351a938c2d9ce
zny2008 <zny2008@02dc478b-fe90-6d34-b7c4-bbeca2ecd945>
2011-08-02 06:29:59 +0000

    ci

--- src/example/fl_module/main.cpp
+++ src/example/fl_module/main.cpp
@@ -21,8 +21,24 @@
 #include <set>
 #include <map>
 using namespace std;
+
+/**
+ * @brief   告诉recv是否继续
+ *
+ * @param   pBuf
+ * @param   bufLen
+ *
+ * @return  >0          已经recv完毕，截取总长度
+ *          =0          继续recv
+ *          <0          协议解析出错
+ */
+int net_handleinput(char* pBuf, int bufLen)
+{
+    return bufLen;
+}
+
 int udp_process(
-        const char* serverIp,int serverPort,unsigned timeoutMs,
+        const char* serverIp,int serverPort,uint32_t timeoutMs,
         const char *sendBuf, int sendLen,
         char *recvBuf,int maxLen,int& recvLen
         )
@@ -58,44 +74,64 @@ int udp_process(
     bzero(&recv_addr,sizeof(recv_addr));
 
     setsockopt(socketFd , SOL_SOCKET,SO_RCVTIMEO,&timeout,sizeof(timeout));
-    ret = recvfrom(socketFd,recvBuf,maxLen,0,(struct sockaddr*)&recv_addr,(socklen_t *) &recv_addr_len);
-    if ( ret <= 0 )
+
+    recvLen = 0;
+    while(true)
     {
-        close(socketFd);
-        return -3;
+        int leftLen = maxLen - recvLen;
+        if (leftLen <= 0)
+        {
+            return -4;
+        }
+        ret = recvfrom(socketFd,recvBuf+recvLen ,leftLen , 0, (struct sockaddr*)&recv_addr,(socklen_t *) &recv_addr_len);
+        if (ret == 0)
+        {
+            close(socketFd);
+            return -5;
+        }
+        else if (ret < 0)
+        {
+            if (errno == EINTR)
+            {
+                continue;
+            }
+            close(socketFd);
+            return -6;
+        }
+
+        recvLen += ret;
+
+        ret = net_handleinput(recvBuf, recvLen);
+        if (ret == 0)
+        {   
+            continue;
+        }   
+        else if (ret < 0)
+        {   
+            close(socketFd);
+            return -7;
+        }   
+        else
+        {   
+            recvLen = ret;
+            break;
+        } 
     }
-    recvLen = ret;
     close(socketFd);
     return 0;
 }
-//我们这里假定返回包中，和pymman那边server相同的协议。即：len(11字节) + data(len)
-//data = cmd(11) + ret(11) + body(len-33)
-int tcp_handleinput(char* pBuf, int bufLen)
-{
-    printf("%d\n",bufLen);
-    return bufLen;
-    if (bufLen < 11)
-    {
-        return -1;
-    }
-    char allLenBuf[12];
-    memcpy(allLenBuf,pBuf,11);
-    allLenBuf[11]='\0';
 
-    int allLen = atoi(allLenBuf);
-    return allLen;
-}
-
-int tcp_connect_poll(const char* serverIp,int serverPort,unsigned timeoutMs)
+int tcp_process(
+        const char* serverIp,int serverPort,uint32_t timeoutMs,
+        const char *sendBuf, int sendLen,
+        char *recvBuf,int maxLen,int& recvLen
+        )
 {
-    struct sockaddr_in serv_addr;
-    bzero(&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port   = htons(serverPort);
-    if (inet_pton(AF_INET, serverIp, &serv_addr.sin_addr) <= 0)
-    {
-        return -1;
-    }
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = timeoutMs*1000;
+
+    int ret;
 
     int socketFd = socket(AF_INET, SOCK_STREAM, 0);
     if ( socketFd < 0 )
@@ -103,84 +139,30 @@ int tcp_connect_poll(const char* serverIp,int serverPort,unsigned timeoutMs)
         return -2;
     }
 
-    //设置非阻塞
-    int val = fcntl(socketFd, F_GETFL, 0);
-    if (val == -1)
+    struct sockaddr_in serv_addr;
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port   = htons(serverPort);
+    if (inet_pton(AF_INET, serverIp, &serv_addr.sin_addr) <= 0)
     {
         close(socketFd);
-        return -3;
+        return -1;
     }
 
-    if (fcntl(socketFd, F_SETFL, val | O_NONBLOCK | O_NDELAY) == -1)
-    {
-        close(socketFd);
-        return -4;
-    }
-
-    int ret;
     ret = connect(socketFd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
     if (ret < 0)
     {
-        if(errno != EINPROGRESS)
-        {
-            close(socketFd);
-            return -1;
-        }
-        else
-        {
-            struct pollfd conncet_client[1];
-            int nfd = 1;
-            memset(&conncet_client[0],0,sizeof(pollfd));
-            conncet_client[0].fd = socketFd;
-            conncet_client[0].events = POLLIN | POLLOUT;
-            int poll_timeout = timeoutMs;
-            ret = ::poll(conncet_client, nfd, poll_timeout);
-            if ( ret <= 0 )
-            {
-                close(socketFd);
-                return -1;
-            }
-            else
-            {
-                if ((conncet_client[0].revents & POLLIN) || (conncet_client[0].revents & POLLOUT))
-                {
-                    int error;
-                    int len = sizeof(error);
-                    int bok = getsockopt(socketFd, SOL_SOCKET, SO_ERROR, &error,(socklen_t*)&len);
-                    if (bok < 0)
-                    {
-                        close(socketFd);
-                        return -1;
-                    }
-                    else if (error)
-                    {
-                        close(socketFd);
-                        return -1;
-                    }
-                }
-                else if ((conncet_client[0].revents & POLLERR) ||
-                        (conncet_client[0].revents & POLLHUP) ||
-                        (conncet_client[0].revents & POLLNVAL))
-                {
-                    close(socketFd);
-                    return -1;
-                }
-            }
-            return socketFd;
-        }
+        close(socketFd);
+        return -2;
     }
-    return socketFd;
-}
-int tcp_send_poll(int socketFd, const char *sendBuf, int sendLen)
-{
-    int ret;
+
     int nsend =0;
     while(1)
     {
         ret = send(socketFd,sendBuf+nsend,(sendLen-nsend),0);
         if (ret <= 0)
         {
-            if ( errno == EINTR || errno == EAGAIN ) //信号中断，或者缓冲区满
+            if (errno == EINTR) //信号中断，或者缓冲区满
                 continue;
             else
                 break;
@@ -191,104 +173,58 @@ int tcp_send_poll(int socketFd, const char *sendBuf, int sendLen)
     }
     if (nsend != sendLen)
     {
-        return -1;
-    }
-    return 0;
-}
-int detect_single_read(int socketFd, unsigned timeoutMs)
-{
-    struct pollfd conncet_client[1];
-    int nfd = 1;
-    memset(&conncet_client[0],0,sizeof(pollfd));
-    conncet_client[0].fd = socketFd;
-    conncet_client[0].events = POLLIN ;
-
-    return poll(conncet_client, nfd, timeoutMs);
-}
-
-int tcp_recv_poll(int socketFd,char *recvBuf,int maxLen,int& recvLen,unsigned timeoutMs)
-{
-    int ret = detect_single_read(socketFd,timeoutMs);
-    if (ret <= 0)
-    {
-        return -2;
-    }
-    ret = recv(socketFd, recvBuf, maxLen, 0);
-    if (ret <= 0)
-    {
         return -3;
     }
-    //已经接受到的数据长度
-    recvLen = ret;
 
-    ret = tcp_handleinput(recvBuf,recvLen);
-    if (ret <= 0)
+    setsockopt(socketFd , SOL_SOCKET,SO_RCVTIMEO,&timeout,sizeof(timeout));
+
+    recvLen = 0;
+    while(true)
     {
-        return -4;
-    }
-
-    int allLen = ret;
-    int iRemain = allLen - recvLen;
-
-    if (allLen > maxLen)
-    {
-        return -5;
-    }
-
-    while (iRemain > 0)
-    {
-        ret = detect_single_read(socketFd,timeoutMs);
-        if(ret<=0)
+        int leftLen = maxLen - recvLen;
+        if (leftLen <= 0)
         {
+            return -4;
+        }
+        ret = recv(socketFd, recvBuf+recvLen, leftLen, 0);
+        if (ret == 0)
+        {
+            close(socketFd);
+            return -5;
+        }
+        else if (ret < 0)
+        {
+            if (errno == EINTR)
+            {
+                continue;
+            }
+            close(socketFd);
             return -6;
         }
 
-        ret = recv(socketFd, recvBuf + recvLen, iRemain, 0);
-        if (ret <= 0)
-        {
-            if ( errno == EINTR || errno == EAGAIN ) //信号中断，或者缓冲区满
-                continue;
-            else
-                break;
-        }
         recvLen += ret;
-        iRemain -= ret;
-    }
-    if (recvLen != allLen)
-    {
-        return -7;
+
+        ret = net_handleinput(recvBuf, recvLen);
+        if (ret == 0)
+        {   
+            continue;
+        }   
+        else if (ret < 0)
+        {   
+            close(socketFd);
+            return -7;
+        }   
+        else
+        {   
+            recvLen = ret;
+            break;
+        } 
     }
 
-    return 0;
-}
-int tcp_process_poll(
-        const char* serverIp,int serverPort,unsigned timeoutMs,
-        const char *sendBuf, int sendLen,
-        char *recvBuf,int maxLen,int& recvLen
-        )
-{
-    int ret;
-    int socketFd = tcp_connect_poll(serverIp,serverPort,timeoutMs);
-    if (socketFd<0)
-    {
-        return -1;
-    }
-
-    ret = tcp_send_poll(socketFd,sendBuf,sendLen);
-    if (ret)
-    {
-        close(socketFd);
-        return -2;
-    }
-    ret = tcp_recv_poll(socketFd, recvBuf, maxLen, recvLen,timeoutMs);
-    if (ret)
-    {
-        close(socketFd);
-        return -3;
-    }
     close(socketFd);
     return 0;
 }
+
 /**
  * @brief   第一次进入so时，需要做的初始化工作，只会执行一次。
  *
@@ -315,7 +251,7 @@ extern "C" int fuload_handle_process(map<string,string>& mapParams)
     string strRecv;
     strRecv.resize(1024);
     int len;
-    int ret =tcp_process_poll(
+    int ret =tcp_process(
             "0.0.0.0",10001,1000,
     //int ret =udp_process(
             //"0.0.0.0",20000,1000,
